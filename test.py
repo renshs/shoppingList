b50ffc907c7f6c00f331d39700c8299ac123fd5f
Kamil <garaeff.camil@yandex.ru>
2020-05-08 16:25:03 +0300

    ready_for_using

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from flask_login import current_user
 from requests import get, post, delete
 
-print(get('http://localhost:5000/api/purchases/5').json())
+print(delete('http://localhost:5000/api/purchases/8').json())
 
 
 # print(post('http://localhost:5000/api/purchases',
